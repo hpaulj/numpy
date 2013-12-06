13ff67fca780a674f2337627a83f9737a8611c16
hpaulj <hpj3@myuw.net>
2013-12-06 15:40:11 -0800

    https://github.com/numpy/numpy/pull/4109 BUG: Fix object scalar return type of 0-d array indices http://stackoverflow.com/questions/20409334/subclassed-empty-numpy-recarray-losing-its-type-and-added-attributes-in-numpy-1

--- numpy/core/src/multiarray/mapping.c
+++ numpy/core/src/multiarray/mapping.c
@@ -1034,7 +1034,8 @@ array_subscript_fromobject(PyArrayObject *self, PyObject *op)
         }
     }
     /* optimization for a tuple of integers */
-    if (PyArray_NDIM(self) > 1 && _is_full_index(op, self)) {
+    //if (PyArray_NDIM(self) > 1 && _is_full_index(op, self)) {
+    if (_is_full_index(op, self)) {
         int ret = _tuple_of_integers(op, vals, PyArray_NDIM(self));
 
         if (ret > 0) {
@@ -1170,7 +1171,17 @@ array_subscript_fromobject(PyArrayObject *self, PyObject *op)
 
     fancy = fancy_indexing_check(op);
     if (fancy != SOBJ_NOTFANCY) {
-        return array_subscript_fancy(self, op, fancy);
+        PyObject *ret;
+        ret = array_subscript_fancy(self, op, fancy);
+
+        if (ret == NULL) {
+            return NULL;
+        }
+        if (PyArray_Check(ret) && PyArray_NDIM((PyArrayObject *)ret) == 0
+                                                 && !_check_ellipses(op)) {
+            return PyArray_Return((PyArrayObject *)ret);
+        }
+        return ret;
     }
     else {
         return array_subscript_simple(self, op, 1);
@@ -1183,7 +1194,7 @@ array_subscript(PyArrayObject *self, PyObject *op)
     int fancy;
     PyObject *ret = NULL;
     if (!PyArray_Check(op)) {
-        ret = array_subscript_fromobject(self, op);
+        return array_subscript_fromobject(self, op);
     }
 
     /* Boolean indexing special case */
@@ -1211,14 +1222,6 @@ array_subscript(PyArrayObject *self, PyObject *op)
         }
     }
 
-    if (ret == NULL) {
-        return NULL;
-    }
-
-    if (PyArray_Check(ret) && PyArray_NDIM((PyArrayObject *)ret) == 0
-                                                && !_check_ellipses(op)) {
-        return PyArray_Return((PyArrayObject *)ret);
-    }
     return ret;
 }
 
